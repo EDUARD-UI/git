5043cfc644ed5c558a46b314254d5810e0c503e4
EDUARD-UI <eduardestf20@gmail.com>
2025-09-14 17:41:55 -0500

    commit avanzado

--- App.java
+++ App.java
@@ -3,5 +3,6 @@ public class App {
     public static void main(String[] args) {
         Persona persona1 = new Persona("superman", "hombre de acero");
         persona1.volar();
+        persona1.getNombre();
     }
 }
