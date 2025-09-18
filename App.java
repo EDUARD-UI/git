dae913fe1bc82896d6d49e21408102e1a74f00f3
EDUARD-UI <eduardestf20@gmail.com>
2025-09-17 20:15:16 -0500

    nuevo personaje

--- App.java
+++ App.java
@@ -2,8 +2,10 @@ public class App {
 
     public static void main(String[] args) {
         Persona persona1 = new Persona("superman", "hombre de acero");
+        Persona persona2 = new Persona("hulk", "verde");
         persona1.volar();
-        persona1.comer();
-        persona1.criticar();
+        persona2.comer();
+
+        System.out.println(persona1);
     }
 }
